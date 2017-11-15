5511dcc726253578621da8d40703f1c7e615fc9e
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-15 12:27:05 +0100

    misc.py: preprocessing - generate information about the questions

--- misc.py
+++ misc.py
@@ -1,5 +1,10 @@
 #!/bin/python3
 
+"""
+    misc.py is a file that contains miscellaneous functions
+    and make preprocessing of data
+"""
+
 from math import sqrt
 import xml.etree.ElementTree as xmlt
 from collections import namedtuple as struct
@@ -32,9 +37,6 @@ def loadTags(file):
     for child in root:
         tset.add(dictToTag(child.attrib))
     return tset
-
-# Variables
-tags = loadTags('data/Tags.xml')
 
 # "public" functions
 def countTags():
@@ -100,18 +102,26 @@ def variance(lst):
     d = [(item - moy) ** 2 for item in lst]
     return (1/ (len(lst)-1)) * sum(d)
 
-def ecarttype(lst):
+def std_deviation(lst):
     return sqrt(variance(lst))
 
+
 def stats_questions():
+    """
+        Preprocessing function that generates basics statistics about
+        the score of the questions posted on stack Overflow from questions.csv.
+
+        return: a n-tuple (min_score, max_score, average_score, variance, standard deviation)
+
+        note: You need to call this function once
+    """
     vmin   = 0
     vmax   = 0
     vtotal = 0
     n      = 0
     vtotal_sq = 0
     with open('data/questions.csv','r') as f:
-        f.readline()
-        #nrow = int(f.readline().strip('\n').split(',')[4])
+        f.readline()    # I ignore the 1st line because is just metadata
         for line in f:
             v = int(line.strip('\n').split(',')[4])
             vmin = v if v < vmin else vmin
@@ -122,10 +132,18 @@ def stats_questions():
             vtotal_sq += v ** 2
         avg = (vtotal / n)
         variance = ((1 / n) * vtotal_sq) - (avg ** 2)
-        return (vmin, vmax, avg, variance, sqrt(variance), 0)
+        return (vmin, vmax, avg, variance, sqrt(variance))
 
-# Variable "globale"
-qstats = stats_questions()
+def write_stats_in(stat, filename):
+    with open(filename, "w+") as f:
+        metadata = 'min, max, average, variance, standard deviation\n'
+        s = str(stat).strip('()') + '\n'
+        f.write(metadata)
+        f.write(s)
+
+# Global variables
+#qstats = stats_questions()
+tags = loadTags('data/Tags.xml')
 
 """
     Test
@@ -134,4 +152,10 @@ qstats = stats_questions()
 #print(getTagInfoByName("java"))
 #print(getTagID("java"))
 #print(getTagName(17))
-print(qstats)
+#print(str((0, 20, 15, 4, 2)).strip('()') )
+#write_stats_in((0, 20, 15, 4, 2), 'question_score_stats.csv')
+"""
+    Real code
+"""
+#print(qstats)
+#write_stats_in(stats_questions(), 'question_score_stats.csv')
