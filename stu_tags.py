8a4429537da2333c8ec5a1b408c316d10fbce4ff
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-17 13:10:31 +0100

    tags in a specific module

--- stu_tags.py
+++ stu_tags.py
@@ -86,63 +86,8 @@ def getTagID(tagname):
     tag = getTagInfoByName(tagname);
     return tag.id if tag is not None else None
 
-"""
-    Stack Overflow - statistics about questions
-"""
-
-def average(lst):
-    return (sum(lst) / len(lst))
-
-def median(lst):
-    lst.sort()
-    return(lst[len(lst)//2])
-
-def variance(lst):
-    moy = average(lst)
-    d = [(item - moy) ** 2 for item in lst]
-    return (1/ (len(lst)-1)) * sum(d)
-
-def std_deviation(lst):
-    return sqrt(variance(lst))
-
-
-def stats_questions():
-    """
-        Preprocessing function that generates basics statistics about
-        the score of the questions posted on stack Overflow from questions.csv.
-
-        return: a n-tuple (min_score, max_score, average_score, variance, standard deviation)
-
-        note: You need to call this function once
-    """
-    vmin   = 0
-    vmax   = 0
-    vtotal = 0
-    n      = 0
-    vtotal_sq = 0
-    with open('data/questions.csv','r') as f:
-        f.readline()    # I ignore the 1st line because is just metadata
-        for line in f:
-            v = int(line.strip('\n').split(',')[4])
-            vmin = v if v < vmin else vmin
-            vmax = v if v > vmax else vmax
-            vtotal += v
-            # Variance
-            n += 1
-            vtotal_sq += v ** 2
-        avg = (vtotal / n)
-        variance = ((1 / n) * vtotal_sq) - (avg ** 2)
-        return (vmin, vmax, avg, variance, sqrt(variance))
-
-def write_stats_in(stat, filename):
-    with open(filename, "w+") as f:
-        metadata = 'min, max, average, variance, standard deviation\n'
-        s = str(stat).strip('()') + '\n'
-        f.write(metadata)
-        f.write(s)
 
 # Global variables
-#qstats = stats_questions()
 tags = loadTags('data/Tags.xml')
 
 """
@@ -152,10 +97,3 @@ tags = loadTags('data/Tags.xml')
 #print(getTagInfoByName("java"))
 #print(getTagID("java"))
 #print(getTagName(17))
-#print(str((0, 20, 15, 4, 2)).strip('()') )
-#write_stats_in((0, 20, 15, 4, 2), 'question_score_stats.csv')
-"""
-    Real code
-"""
-#print(qstats)
-#write_stats_in(stats_questions(), 'question_score_stats.csv')
