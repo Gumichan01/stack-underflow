a0e38eee4c0b1bf0fdf2ece7233c0e697dd8a842
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-29 11:35:30 +0100

    test.py: test file

---
+++ test.py
@@ -0,0 +1,13 @@
+
+import csv;
+
+#reader = csv.reader(['1997,Ford,E350,"Super, luxurious truck"'], skipinitialspace=True)
+#for r in reader:
+#    print(r)
+
+with open('data/test.csv') as csvf:
+    reader = csv.reader(csvf, skipinitialspace=True)
+    for r in reader:
+        r[6] = ''.join(' ' if c == '\n' or c == '\t' else c for c in r[6]).strip(' ')
+        print(len(r))
+        print(r)
