a40d1be77dba8b3339c34e0725217ef36e2c8b7b
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-30 13:42:25 +0100

    test.py: update
    It seems that creating a csv.reader object is not heavy.
    So I can can easily create a csv.reader object of a huge file.
    
    I am also starting to retrieve the content of the questions.
    
    --

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 
-import csv;
+import csv
+from stu_tags import *
+from stu_questions import *
 
 #reader = csv.reader(['1997,Ford,E350,"Super, luxurious truck"'], skipinitialspace=True)
 #for r in reader:
 #    print(r)
-
-with open('data/test.csv') as csvf:
+"""
+with open('data/sample_questions.csv') as csvf:
     reader = csv.reader(csvf, skipinitialspace=True)
     for r in reader:
-        r[6] = ''.join(' ' if c == '\n' or c == '\t' else c for c in r[6])
-        print(len(r))
-        print(r)
+        #r[6] = ''.join(' ' if c == '\n' or c == '\t' else c for c in r[6])
+        #print(len(r))
+        print(r[0])
+    print(v)
+"""
+
+tgs = getTagsOfCluster(1)
+print(tgs)
+q = getQuestions(tgs[1])
+print(len(q))
