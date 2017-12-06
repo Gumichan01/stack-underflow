20223b9196ae6bb79cadc5469d175920760f953f
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-06 12:18:46 +0100

    don't take care of that

--- test.py
+++ test.py
@@ -3,6 +3,16 @@ import csv
 from stu_tags import *
 from stu_questions import *
 
+
+# another experiment
+
+
+
+
+
+
+
+
 """
 with open('data/sample_questions.csv') as csvf:
     reader = csv.reader(csvf, skipinitialspace=True)
@@ -12,6 +22,9 @@ with open('data/sample_questions.csv') as csvf:
         print(r[0])
     print(v)
 """
+
+# experiment
+
 """
 print('Tags')
 tgs = getTagsOfCluster(1)
@@ -35,7 +48,11 @@ else:
     print("not found question")
 """
 
+# nothing
+
+"""
 with open('data/sample_questionsv0.csv', 'r') as csvf:
     reader = csv.reader(csvf, skipinitialspace=True)
     for r in reader:
         print(r[0])
+"""
