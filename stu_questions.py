30ca6c9d436237b9a47985aa8b1d41f0569cce83
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-08 12:34:37 +0100

    stu_questions.py: clean the code

--- stu_questions.py
+++ stu_questions.py
@@ -7,45 +7,10 @@
 
 from stu_tags import *
 from math import sqrt
+import stu_misc
 
 QUESTION_TAGS = 'data/question_tags.csv'
 SAMPLE_QUESTIONS = 'data/sample_questionsv0.csv'
-
-def _stats_questions():
-    """
-        Preprocessing function that generates basics statistics about
-        the score of the questions posted on stack Overflow from questions.csv.
-
-        return: a n-tuple (min_score, max_score, average_score, variance, standard deviation)
-
-        note: You need to call this function once
-    """
-    vmin   = 0
-    vmax   = 0
-    vtotal = 0
-    n      = 0
-    vtotal_sq = 0
-    with open('data/questions.csv','r') as f:
-        f.readline()    # I ignore the 1st line because is just metadata
-        for line in f:
-            v = int(line.strip('\n').split(',')[4])
-            vmin = v if v < vmin else vmin
-            vmax = v if v > vmax else vmax
-            vtotal += v
-            # Variance
-            n += 1
-            vtotal_sq += v ** 2
-        avg = (vtotal / n)
-        variance = ((1 / n) * vtotal_sq) - (avg ** 2)
-        return (vmin, vmax, avg, variance, sqrt(variance))
-
-def _write_stats_in(stat, filename):
-    with open(filename, "w+") as f:
-        metadata = 'min, max, average, variance, standard deviation\n'
-        s = str(stat).strip('()') + '\n'
-        f.write(metadata)
-        f.write(s)
-
 
 def getQTags(question_id):
     """
@@ -56,51 +21,40 @@ def getQTags(question_id):
         Return:
             the list of tags if found, None otherwise
     """
-    tags = []
+    qtags = []
     with open(QUESTION_TAGS, 'r') as f:
         f.readline()    # I ignore this first line because it's just metadata
         for line in f:
-            row = line.strip('\n').split(',')
+            row = line.strip(stu_misc.ENDL).split(stu_misc.COMMA)
             qid = int(row[0])
             if qid == question_id:
                 tags.append(row[1])
             elif qid > question_id:
-                return tags if tags != [] else None
-        return tags if tags != [] else None
+                return qtags if qtags != [] else None
+        return qtags if qtags != [] else None
 
-def getQuestions(tagname):
+def getQuestionsFromTag(tagname):
     """
         Return the list of the questions that are related to the tags
 
         Arg:
-            tagname the name of the tag
+            the name of the tag
         Return:
             the list of question, by identifier
     """
-    tags = []
+    qarray = []
     with open(QUESTION_TAGS, 'r') as f:
         f.readline()    # I ignore this first line because it's just metadata
         for line in f:
-            row = line.strip('\n').split(',')
+            row = line.strip(stu_misc.ENDL).split(stu_misc.COMMA)
             if row[1] == tagname:
-                tags.append(int(row[0]))
-    return tags
+                qarray.append(int(row[0]))
+    return qarray
 
 """
     Test
 """
 
-"""
-print(len(getQuestions('c++11')))
-
-t = getQTags(698)
-
-for telem in t:
-    cluster = getTagClusterByName(telem)
-    print('------------------------')
-    print(telem)
-    print(cluster)
-    print(getTagInfoByName(telem))
-    if cluster is not None:
-        print(getTagsOfCluster(cluster))
-"""
+print('Questions from c++')
+qs = getQuestionsFromTag('c++')
+print(len(qs))
