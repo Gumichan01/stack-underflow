e21a861528433f005108bcbe4efae12b29452d4b
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-09 20:38:29 +0100

    (-.-)

--- test.py
+++ test.py
@@ -1,51 +1,36 @@
 
 import csv
-from stu_tags import *
-from stu_questions import *
+#from stu_tags import *
+#from stu_questions import *
+
+"""
+reader = csv.reader(['ID,NAME,TEXT', '1,jello,gumi', '2,shdsdbkzd,jzfkzj'], skipinitialspace=True)
+for r in reader:
+    print(r)
+"""
 
 
 # another experiment
 
-
-
-
-
-
-
-
+#
 """
-with open('data/sample_questions.csv') as csvf:
-    reader = csv.reader(csvf, skipinitialspace=True)
-    for r in reader:
-        #r[6] = ''.join(' ' if c == '\n' or c == '\t' else c for c in r[6])
-        #print(len(r))
-        print(r[0])
-    print(v)
-"""
+def isSorted():
+    with open('data/sample_questionsv0.csv') as csvf:
+        v = 0
+        qid = 0
+        qidp = 0
+        reader = csv.reader(csvf, skipinitialspace=True)
+        for r in reader:
+            if r[0] == 'Id':
+                continue
 
-# experiment
-
-"""
-print('Tags')
-tgs = getTagsOfCluster(1)
-print(tgs)
-q = getQuestions(tgs[1])
-print(len(q))
-s = None
-print('get the question no. ', q[0])
-with open('data/sample_questionsv0.csv', 'r') as csvf:
-    reader = csv.reader(csvf, skipinitialspace=True)
-    for r in reader:
-        if r[0] == 'Id':
-            continue
-        r[6] = ''.join(' ' if c == '\n' or c == '\t' else c for c in r[6])
-        if int(r[0]) == q[0]:
-            s = ''.join(r[5], r[6])
-
-if s is not None:
-    print(s)
-else:
-    print("not found question")
+            qid = int(r[0])
+            if v == 0 or qidp <= qid:
+                qidp = qid
+                v += 1
+            elif qid < qidp:
+                    return False
+        return True
 """
 
 # nothing
