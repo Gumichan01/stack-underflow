fbc754d5d1a3fc4d5b548cc1c4dae5e248c0fb64
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-12 09:49:14 +0100

    stu_dictionary.py: generate the tags and the world set associated

--- stu_dictionary.py
+++ stu_dictionary.py
@@ -10,32 +10,42 @@
 from stu_questions import *
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+DICT_FILE='dict.csv'
+
 def transform_text(documents):
     tfidf_model = TfidfVectorizer(min_df = 0.1, max_df = 0.6, stop_words='english')
     tfidf = tfidf_model.fit_transform(documents)
     return tfidf_model.get_feature_names()
 
-"""
+def generate_string(wlist):
+    return ' '.join([w for w in wlist])
+
+tags = ['c++','java','sql','python','scala','windows','linux','unix','c#','c']
 print('Number of tags to process: ', len(tags))
 
-for t in tags:
-    print('Questions from ', t)
-    qs = getQuestionsFromTag(t)
+with open(DICT_FILE, 'w+') as f:
+    print('Generate the tags into ', DICT_FILE,' ...')
+    for t in tags:
+        print('Questions from ', t)
+        qs = getQuestionsFromTag(t)
 
-    # I don't take tags with less than 10 questions that use it
-    n = len(qs)
-    if(n < 100):
-        print('FAILURE - ',t, ' ignored: not enough questions: ', n)
-        continue
-    else:
-        print('SUCCESS - ',t, ' has ', len(qs), ' questions')
-        fwords = transform_text(getDocuments(qs))
-        print('Number of words: ', len(fwords))
-        fwords = None
-        #print(fwords)
-        # put it in a file
+        # I don't take tags with less than 10 questions that use it
+        n = len(qs)
+        if(n < 100):
+            print('FAILURE - ',t, ' ignored: not enough questions: ', n)
+            continue
+        else:
+            print('SUCCESS - ', t, ' has ', len(qs), ' questions')
+            fwords = transform_text(getDocuments(qs))
+            print('Number of words: ', len(fwords))
+            # put it in a file
+            f.write(t)
+            f.write(',')
+            f.write(generate_string(fwords))
+            f.write('\n')
+    print('Generated every tags ...')
+
 """
-
 print('Questions from c++')
 qs = getQuestionsFromTag('c++')
 print('number of questions')
@@ -48,3 +58,4 @@ print(len(qdoc))
 fwords = transform_text(qdoc)
 print('number of world: ', len(fwords))
 print(fwords)
+"""
