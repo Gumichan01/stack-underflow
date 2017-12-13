3721d95b4d02cce553b3fc06eeffd53d3ac8e115
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-12-13 09:47:06 +0100

    stu_dictionary: formatted the source code

--- stu_dictionary.py
+++ stu_dictionary.py
@@ -13,11 +13,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 def transform_text(documents):
+    """
+        Calculate the TF-IDF of the documents
+
+        Arg:
+            a list of documents
+        Return:
+            the feature names from the documents
+    """
     tfidf_model = TfidfVectorizer(min_df = 0.1, max_df = 0.6, stop_words='english')
     tfidf = tfidf_model.fit_transform(documents)
     return tfidf_model.get_feature_names()
 
-def generate_string(wlist):
+def generate_string_from(wlist):
+    """
+        Convert list of element to a string that conains every elements
+    """
     return ' '.join([w for w in wlist])
 
 # sample of tags (for testing)
@@ -26,30 +37,32 @@ max_t = 100
 print('Number of tags to process: ', len(tags))
 print('Maximum number of tags to process: ', max_t)
 
+
 v = 0
-with open(stu_misc.DICT_FILE, 'w+') as f:
+max_v = 4
+with open('stu_misc.DICT_FILE, 'w+') as f:
     print('Generate the tags into ',stu_misc.DICT_FILE,'...')
     for t in tags:
-        if v == 100 :
+        if v == max_v :
             break
 
         # process the tag
         print('Questions from ', t)
-        qs = getQuestionsFromTag(t)
+        qs = get_question_from(t)
 
         # I don't take tags with less than 10 questions that use it
         n = len(qs)
-        if(n < 100):
+        if(n < max_t):
             print('FAILURE - ',t, ' ignored: not enough questions: ', n)
             continue
         else:
             print('SUCCESS - ', t, ' has ', len(qs), ' questions')
-            fwords = transform_text(getDocuments(qs))
+            fwords = transform_text(get_documents(qs))
             print('Number of words: ', len(fwords))
             # put it in a file
             f.write(t)
             f.write(',')
-            f.write(generate_string(fwords))
+            f.write(generate_string_from(fwords))
             f.write('\n')
             v += 1
             print('Tags that has been read: ', v)
@@ -57,13 +70,13 @@ with open(stu_misc.DICT_FILE, 'w+') as f:
 
 """
 print('Questions from c++')
-qs = getQuestionsFromTag('c++')
+qs = get_question_from('c++')
 print('number of questions')
 print(len(qs))
-#print('check')
-#print(all(q >= stu_misc.MIN_ID and q <= stu_misc.MAX_ID and (q % 10) == 0 for q in qs))
+print('check')
+print(all(q >= stu_misc.MIN_ID and q <= stu_misc.MAX_ID and (q % 10) == 0 for q in qs))
 print('get documents')
-qdoc = getDocuments(qs)
+qdoc = get_documents(qs)
 print(len(qdoc))
 fwords = transform_text(qdoc)
 print('number of world: ', len(fwords))
