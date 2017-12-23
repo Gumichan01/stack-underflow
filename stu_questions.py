b71c921ed1e9ec6e09b846020cce2de89dec11d2
Gumichan01 <luxon.jean.pierre@gmail.com>
2017-12-23 12:40:40 +0100

    stu_questions.py: load the lookup table(question_tags.csv)

--- stu_questions.py
+++ stu_questions.py
@@ -20,14 +20,43 @@ def _load_questions():
     with open(QUESTION_SAMPLE) as f:
         f.readline()            # Ignore the first line
         reader = csv.reader(f.readlines(), skipinitialspace=True)
-        qarray = []
+        qdict = dict()
         for r in reader:
-            qarray.append( (int(r[0]), stu_misc.concatenate(r[5], r[6])) )
+            qdict[int(r[0])] = stu_misc.concatenate(r[5], r[6])
         print('Done')
-        return qarray
+        return qdict
+
+def _load_lookup_heavy():
+    print('Load lookup table from ',QUESTION_TAGS,'...')
+    ldict = dict()
+    with open(QUESTION_TAGS, 'r') as f:
+        f.readline()    # I ignore this first line because it's just metadata
+        for line in f:
+            row = line.strip(stu_misc.ENDL).split(stu_misc.COMMA)
+            qidv = int(row[0])
+            tgname = row[1]
+            # A new tag
+            if ldict.get(tgname) is None:
+                ldict[tgname] = []
+
+            # Add the identifier of the question in the tag
+            if qidv >= stu_misc.MIN_ID and qidv <= stu_misc.MAX_ID and (qidv % 10) == 0 and is_valid(qidv):
+                ldict[tgname].append(qidv)
+    print('Done')
+    return ldict
+
+def _load_lookup():
+    with open('lookup.csv','r') as f:
+        print('Load generated lookup table from ...')
+        ldict = dict()
+        for line in f.readlines():
+            row  = line.strip(stu_misc.ENDL).split(stu_misc.COMMA)
+            l.dict[row[0]] = [int(v) for v in row[1].split(' ')]
+    return ldict
+
 
 # public functions
-def get_question_from(tagname):
+def _get_question_from(tagname):
     """
         Return the list of the questions that are related to the tags
 
@@ -63,9 +92,9 @@ def is_valid(qid):
         Return:
             True if it is in the sample, False otherwise
     """
-    return stu_misc.dichotomic_search([q[0] for q in questions], qid)
+    return questions.get(qid) is not None
 
-def filter(qarray):
+def _filter(qarray):
     """
         Filter the array of question identifiers.
         It generates a new array that contains the identifiers
@@ -88,32 +117,49 @@ def get_documents(idqarray):
         Return:
             the list of documents (the questions)
     """
+    if idqarray == []:
+        return []
+
     documents = []
-    qids = [q[0] for q in questions]
+    #qids = [q[0] for q in questions]
     for id in idqarray:
-        qindex = stu_misc.dichotomic_find(qids, id)
-        if qindex is not None:
-            documents.append(questions[qindex][1])
+        #qindex = stu_misc.dichotomic_find(qids, id)
+        if questions.get(id) is not None:
+            documents.append(questions[id])
     return documents
 
 # Global variable
 questions = _load_questions()
+lookup_table = _load_lookup_heavy()
+
+print('Save lookup')
+with open('lookup.csv', 'w+') as f:
+    for t in lookup_table:
+        if lookup_table[t] == []:
+            continue
+
+        f.write(t)
+        f.write(',')
+        f.write(' '.join([str(w) for w in lookup_table[t]]))
+        f.write('\n')
+
+print('Lookup saved')
 
 """
     Test
 """
 
-#print(len(questions))
-#print(questions[0])
-"""
+print(len(questions))
+print(len(lookup_table))
+#print(questions[80])
+
 print('Questions from c++')
-qs = get_question_from('c++')
+#qs = get_question_from('c++')
 print('number of questions')
-print(len(qs))
+print(len(lookup_table['c++']))
 print('check')
-print(all(q >= stu_misc.MIN_ID and q <= stu_misc.MAX_ID and (q % 10) == 0 for q in qs))
+#print(all(q >= stu_misc.MIN_ID and q <= stu_misc.MAX_ID and (q % 10) == 0 for q in qs))
 print('get documents')
-qdoc = get_documents(qs)
+qdoc = get_documents(lookup_table['c++'])
 print(len(qdoc))
-print(filter([75,76,77,78,79,80, 330, 331, 332]))
-"""
+#print(filter([75,76,77,78,79,80, 330, 331, 332]))
